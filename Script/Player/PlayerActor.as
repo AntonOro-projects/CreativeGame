d9c40888901b3f89704c748a1373be95e8520474
Anton <oroanton@gmail.com>
2025-08-09 13:15:49 +0200

    Initial commit project structure & vscode setup

---
+++ Script/Player/PlayerActor.as
@@ -0,0 +1,4 @@
+class APlayer : AActor
+{
+
+};
---
+++ Source/CreativeGame/CreativeGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CreativeGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CreativeGame, "CreativeGame" );
